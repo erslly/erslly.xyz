9f49cd937cd58bdbb3bd96669e222c0b5bef5bc8
erslly <157350349+erslly@users.noreply.github.com>
2024-10-23 20:57:20 +0300

    Add files via upload

---
+++ ersllydev/script.js
@@ -0,0 +1,18 @@
+var typed = new Typed(".typewriter-text",
+    {
+        strings: ["DEVELOPER", "WEB DESIGNER", "BOT DEVELOPER"],
+        typeSpeed: 100,
+        backSpeed: 50,
+        loop: true
+    }
+)
+
+
+
+
+
+
+
+
+
+
